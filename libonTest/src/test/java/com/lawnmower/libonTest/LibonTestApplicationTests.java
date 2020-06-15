ba2dd64d4d18321a7728297e38f81adb4e2ccc9f
bertgoulois <bertrandgoulois@gmail.com>
2020-06-15 19:00:45 +0200

    Commit 1 :  Initialisation du projet (import pom.xml généré avec spring initializr)

---
+++ libonTest/src/test/java/com/lawnmower/libonTest/LibonTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lawnmower.libonTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibonTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
