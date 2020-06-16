c4249339f9f6ff1c7784a5763cd78bebe5d99ad2
bertgoulois <bertrandgoulois@gmail.com>
2020-06-16 11:28:40 +0200

    Commit 9 : Ajout de tests unitaires (mock, test du payload de l'énoncé en test unitaire) + modif reste à faire (amélioration du code)

--- libonTest/src/test/java/com/lawnmower/libonTest/service/LawnMowerMovementsServiceTest.java
+++ libonTest/src/test/java/com/lawnmower/libonTest/service/LawnMowerMovementsServiceTest.java
@@ -1,14 +1,40 @@
 package com.lawnmower.libonTest.service;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
+import com.lawnmower.libonTest.dto.LawnMowerJsonObject;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 class LawnMowerMovementsServiceTest {
+	
+	@Autowired
+	private LawnMowerMovementsService lawnMowerMovementsService;
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testExecuteOrdersFromPayload() throws Exception{
+		LawnMowerJsonObject lawnMowerJsonObjectTest = new LawnMowerJsonObject("5 5\r\n1 2 N\r\nGAGAGAGAA\r\n3 3 E\r\nAADAADADDA");
+		LawnMowerJsonObject lawnMowerJsonObjectExpected = new LawnMowerJsonObject("1 3 N\r\n5 1 E");
+		LawnMowerJsonObject lawnMowerJsonObjectReturnedByService = lawnMowerMovementsService.executeOrdersFromPayload(lawnMowerJsonObjectTest);
+		assertEquals(lawnMowerJsonObjectExpected.getPayLoad(), lawnMowerJsonObjectReturnedByService.getPayLoad());
+	}
+	
+	@Test
+	void testExecuteOrdersFromPayloadShowsIKnowMockito() throws Exception{
+		LawnMowerJsonObject lawnMowerJsonObjectTest = new LawnMowerJsonObject("5 5\r\n1 2 N\r\nGAGAGAGAA\r\n3 3 E\r\nAADAADADDA");
+		LawnMowerJsonObject lawnMowerJsonObjectExpected = new LawnMowerJsonObject("1 3 N\r\n5 1 E");
+		LawnMowerMovementsService lawnMowerMovementsServiceForMock = Mockito.mock(LawnMowerMovementsService.class);
+		when(lawnMowerMovementsServiceForMock.executeOrdersFromPayload(lawnMowerJsonObjectTest)).thenReturn(lawnMowerJsonObjectExpected);
+		LawnMowerJsonObject lawnMowerJsonObjectReturnedByService = lawnMowerMovementsServiceForMock.executeOrdersFromPayload(lawnMowerJsonObjectTest);
+		assertEquals(lawnMowerJsonObjectExpected.getPayLoad(), lawnMowerJsonObjectReturnedByService.getPayLoad());
 	}
 
 }
